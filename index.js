fb10ef1c08e2a129e579b32c9399be7c10ee0629
Ryan Roat <ryanroat@gmail.com>
2018-01-16 21:54:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget () {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementsByClassName('ranked-list');
+  let oldNum, newNum;
+  
+  // console.log(lis);
+  
+  for (let i = 0; i < lis.length; i++) {
+    
+    oldNum = parseInt(lis[i].innerHTML);  // pull innerHTML out as an int
+    newNum = oldNum + n;                  // increase and store in newNum
+    
+    lis[i].innerHTML = newNum.toString();
+  }
+}
+
+function deepestChild() {
+  
+}
